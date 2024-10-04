92ed0202832306a77f61474b6c4785da0277c50c
SnortinAcidTabz <Bradleyh714@proton.me>
2024-10-04 15:12:24 -0700

    main.py

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ Bot = commands.Bot(command_prefix='!', intents=intents)
 
 @Bot.event
 async def on_ready():
-    print(f'Bot {Bot.user} is ready!')
+    print(f'{Bot.user} *your ready prompt here*')
 
 @Bot.event
 async def on_member_join(member):
     channel = Bot.get_channel(*your channel id here*)
     if channel:
         embed = discord.Embed(
-            title=f'Welcome {member.name} your the sluttiest of sluts',
-            description=f'Dont be a fucktard and have fun {member.mention}!',
+            title=f'Welcome {member.name} *your title here*',
+            description=f'*your description here* {member.mention}',
             color=discord.Color.*your color here*()
         )
 
